1b821a9cb50e7f99d5cc13f3dcc2143423b8fbb4
ryan-neal <ryan.neal0@gmail.com>
2017-02-18 09:49:19 -0800

    message

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a radius");
+            String radius = Console.ReadLine();
+            double radius2 = Double.Parse(radius);
+            double answer = Math.PI * Math.Pow(radius2, 2.0);
+            Console.WriteLine(answer);
+            Console.ReadLine();
+
+        }
+    }
+}
